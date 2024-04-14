ea0c7364eca102d3864ab26d3d4b9be770b1e7be
Ryota <mushroominger@gmail.com>
2024-04-14 11:02:28 +0900

    setup.sh初回コミット

---
+++ setup.sh
@@ -0,0 +1,16 @@
+brew install --cask alfred
+brew install --cask appcleaner
+brew install --cask Bitwarden
+brew install --cask cloudmounter
+# brew install --cask hiddenbar
+brew install --cask karabiner-elements
+brew install --cask maccy
+brew install --cask rectangle
+brew install --cask google-chrome
+brew install --cask affinity-designer
+brew install --cask affinity-photo
+brew install --cask blender
+brew install --cask discord
+brew install --cask messenger
+brew install --cask pixelsnap
+brew install --cask visual-studio-code
