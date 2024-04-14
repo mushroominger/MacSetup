9d30e591c88177ffc6c0525105108111c5d34e2c
Ryota <mushroominger@gmail.com>
2024-04-14 12:45:55 +0900

    更新

--- setup.sh
+++ setup.sh
@@ -3,13 +3,14 @@ brew install --cask appcleaner
 brew install --cask Bitwarden
 brew install --cask cloudmounter
 # brew install --cask hiddenbar
-brew install --cask karabiner-elements
-brew install --cask maccy
+# brew install --cask karabiner-elements
+# brew install --cask maccy
 brew install --cask rectangle
 brew install --cask google-chrome
 brew install --cask affinity-designer
 brew install --cask affinity-photo
 brew install --cask blender
+brew install --cask deepl
 brew install --cask discord
 brew install --cask messenger
 brew install --cask pixelsnap
